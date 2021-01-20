afb39915b9bba6b9a300ed768c45c16d0dc00ca3
Megan Lavallee <megan.lavallee@yahoo.com>
2021-01-19 18:50:35 -0700

    server run through

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const app = express()
+var myLogger = function (req, res, next) {
+  console.log("logged")
+  next()
+}
+app.use(myLogger)
+app.get('/', function (req, res){
+  res.send("sent")
+})
 
+app.listen(8080)
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-const app = express();
+// const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -23,14 +33,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
  require('./routes/api-routes.js')(app);
  require('./routes/html-routes.js')(app);
+ require('./seeders/seed.js');
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/Controller.js");
+// const routes = require("./seeders/seed.js");
 
-app.use(routes);
+// app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+// app.listen(PORT, function() {
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
